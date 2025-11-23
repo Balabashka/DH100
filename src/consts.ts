72806b78a90380d96799a1ed887573571d72aaf8
Balabashka <balabanov.danil06@gmail.com>
2025-11-23 07:39:45 -0500

    Initial commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "AI & Creativity Project";
+export const SITE_DESCRIPTION =
+  "A digital humanities project exploring how AI impacts art, music, and human expression.";
